64a95f82125bf5d79cd5197e4f48f50cc0a2c118
Y-dash <yokokura-yusuke@y-dash.net>
2020-07-30 17:42:17 +0900

    init project

---
+++ src/main/java/net/y_dash/minecraft/ban_by_tnt_plugin/banbytntplugin/BanByTntPlugin.java
@@ -0,0 +1,17 @@
+package net.y_dash.minecraft.ban_by_tnt_plugin.banbytntplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BanByTntPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
